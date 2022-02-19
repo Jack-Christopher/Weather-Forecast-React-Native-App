7d42f5c5ce989e5713a9a019b511619a45982ae0
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-02-19 10:35:06 -0500

    Settings updated

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from './screens/HomeScreen';
-import Start from './screens/StartScreen';
+import LocalWeather from './screens/LocalWeatherScreen';
+import SelectedPlaceWeather from './screens/SelectedPlaceWeatherScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,7 +12,8 @@ function MyStack()
     return(
         <Stack.Navigator>
             <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Start" component={Start} />
+            <Stack.Screen name="LocalWeather" component={LocalWeather} />
+            <Stack.Screen name="SelectedPlaceWeather" component={SelectedPlaceWeather} />
         </Stack.Navigator>
     );
 }
